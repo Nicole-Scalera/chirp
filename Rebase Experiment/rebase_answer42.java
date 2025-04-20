8f8f154ee7705d30bece9958d7b42dbaf0fda271
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-04-20 15:47:46 -0400

    Created rebase_answer42.java
    Overview
    - Created rebase_answer42.java.
    
    In-depth Details
    - This is directly related to #11 (sub issue of #10).
    - I am making some commits on the main branch after creating a new feature branch for #11.
    - I will make another commit as well to then view, which rebase_helloworld.java can rebase to catch up to.
         - rebase_helloworld.java created in 6881716.

---
+++ Rebase Experiment/rebase_answer42.java
@@ -0,0 +1,8 @@
+public class rebase_answer42
+{
+    public static void main(String[] args)
+    {
+        System.out.println("The answer to life, the universe, and everything is... 42.");
+    }
+}
+
