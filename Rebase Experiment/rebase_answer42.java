d719d6c21899d700980d4421d60acf259b41526d
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-04-20 16:10:36 -0400

    Updated rebase_answer42.java
    Overview
    - This commit directly addresses #10.
    
    In-depth Details
    - Updated rebase_answer42.java, which was created in 8f8f154.
    - The program now asks the user to guess the answer to life, the universe, and everything.
         - Answer is, of course, 42.
    - Until the user answers correctly, the program will loop.
    - This is the second commit to off-set the base point of the feature branch.
         - i.e. these changes are not present there.
    - I will now attempt to make changes on the feature branch, which can then be rebased with respect to main.

--- Rebase Experiment/rebase_answer42.java
+++ Rebase Experiment/rebase_answer42.java
@@ -1,8 +1,39 @@
+import java.util.Scanner;
+
 public class rebase_answer42
 {
     public static void main(String[] args)
     {
-        System.out.println("The answer to life, the universe, and everything is... 42.");
+        // Create a scanner named input
+        Scanner input = new Scanner(System.in);
+
+        // Prompt user for answer
+        System.out.println("What is the answer to life, the universe, and everything?");
+
+        int number = 42; // Answer
+        int userNumber = 0; // Store user's guesses
+        
+        // Loop until answer is correct
+        while (userNumber != number) {
+            
+            // Prompt the user to guess the number
+            System.out.print("\nYour Answer: ");
+            
+            // Grab answer
+            userNumber = input.nextInt();
+            
+            // Check answer
+            if (userNumber == number) {
+                System.out.println("Yes, the number is " + number + ".");
+            } else if (userNumber > number) {
+                System.out.println("Your guess is too high");
+            } else {
+                System.out.println("Your guess is too low");
+            }
+        }
+
+        // Close the scanner
+        input.close();
     }
 }
 
