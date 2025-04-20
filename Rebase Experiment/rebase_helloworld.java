68817160aefec600d59edfdf0ea97da581749ea0
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-04-20 15:04:45 -0400

    Created rebase_helloworld.java
    Overview
    - Created "rebase_helloworld.java" file in new Rebase Experiment folder.
    
    In-depth Details
    - This commit directly addresses the experiment I outlined in #10.
    - I am creating this standard hello world file to play around with in terms of rebasing using GitKraken.
    - I plan on following this tutorial on the GitKraken website: https://www.gitkraken.com/learn/git/git-rebase#how-to-git-rebase-with-gitkraken-client

---
+++ Rebase Experiment/rebase_helloworld.java
@@ -0,0 +1,10 @@
+// Standard hello world function in java
+
+public class rebase_helloworld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
+
